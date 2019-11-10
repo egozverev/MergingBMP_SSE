efce8695fd966ac54fdfe2cc786ffb3532d999d4
egor <zzwerruga@mail.ru>
2019-11-10 21:27:48 +0300

    No-SSE working ver

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <strings.h>
 #include <stdlib.h>
 #include <stdint.h>
-
+#include <math.h>
 #include <emmintrin.h> //SSE2 intrinsics
 #include <mmintrin.h> // MMX (ancestor of SSE ) intrinsics
 #include <smmintrin.h> //SSE 4.1 intrinsics
@@ -25,13 +25,9 @@ typedef struct {             // Total: 54 bytes
     uint32_t num_colors;       // Number of colors
     uint32_t important_colors; // Important colors
 } __attribute__ ((packed)) BMPHeader;
-typedef struct {
-    BMPHeader header;
-    unsigned char *data;
-} BMPImage;
 
 
-unsigned char *LoadBitmapFile(char *filename, BMPHeader *bmp_header) {
+unsigned char *LoadBitmapFile(const char *filename) {
     FILE *file_ptr;
     unsigned char *bmp_image_data;  // image data pointer, returned in the end
 
@@ -40,14 +36,20 @@ unsigned char *LoadBitmapFile(char *filename, BMPHeader *bmp_header) {
         return NULL;
     }
     //read header first
-    fread(bmp_header, sizeof(BMPHeader), 1, file_ptr);
+
+    fseek(file_ptr, 0, SEEK_END); /* teleport to the end of the file */
+    uint64_t file_length = ftell(file_ptr);
+    fseek(file_ptr, 0, SEEK_SET); /* teleport to the beginning */
+
+
+    //fread(bmp_header, sizeof(BMPHeader), 1, file_ptr);
 
 
     //just check that this is bmp file - using some kind of magic value
-    if (bmp_header->type != 0x4D42) {
+    /*if (bmp_header->type != 0x4D42) {
         fclose(file_ptr);
         return NULL;
-    }
+    }*/
 
 
     //fseek(file_ptr, bmp_header->offset, SEEK_SET);
@@ -55,123 +57,95 @@ unsigned char *LoadBitmapFile(char *filename, BMPHeader *bmp_header) {
     //fread(bmp_trash_data, bmp_header->offset - sizeof(BMPHeader), 1, file_ptr);
     //_mm_free(bmp_trash_data);
     //allocation of the memory
-    bmp_image_data = (unsigned char *) _mm_malloc(bmp_header->image_size_bytes, 16);
+    //bmp_image_data = (unsigned char *) _mm_malloc(bmp_header->image_size_bytes, 16);
     // special malloc -
-
+    unsigned char *file_all_data = (unsigned char *) _mm_malloc(file_length, 16);
     //check if malloc went wrong
-    if (!bmp_image_data) {
+    /*if (!bmp_image_data) {
         free(bmp_image_data);
         fclose(file_ptr);
         return NULL;
-    }
+    }*/
 
     //read in the bitmap image data
-    fread(bmp_image_data, bmp_header->image_size_bytes + bmp_header->offset - sizeof(BMPHeader), 1, file_ptr);
+    fread(file_all_data, file_length, 1, file_ptr);
 
 
 
 
     //close file and return bitmap iamge data
     fclose(file_ptr);
-    return bmp_image_data;
+    return file_all_data;
 
 
 }
 
-BMPImage ReadBMPImage(char *filename) {
-    BMPImage bmp_image;
-    bmp_image.data = LoadBitmapFile(filename, &bmp_image.header);
-    return bmp_image;
-}
 
-
-void MergeBMPImages(const BMPImage *fst_image, const BMPImage *snd_image, char *result_filename) {
+void MergeBMPImages(const unsigned char *all_data1, const unsigned char *all_data2, const char *result_filename) {
     FILE *file_ptr = fopen(result_filename, "wb");
-    fwrite(&fst_image->header, sizeof(BMPHeader), 1, file_ptr);
-    fwrite(fst_image->data,fst_image->header.offset - sizeof(BMPHeader), 1, file_ptr);
+    BMPHeader *header1 = (BMPHeader *) all_data1;
+    BMPHeader *header2 = (BMPHeader *) all_data2;
+    fwrite(all_data1, header1->offset, 1, file_ptr);
 
     //unsigned char * bmp_trash_data  = (unsigned char *) _mm_malloc(fst_image->header.offset - sizeof(BMPHeader), 16);
     //fread(bmp_trash_data, fst_image->header.offset - sizeof(BMPHeader), 1, file_ptr);
     //_mm_free(bmp_trash_data);
-    unsigned char *data1 = fst_image->data + (fst_image->header.offset - sizeof(BMPHeader));
-    unsigned char *data2 = snd_image->data + (snd_image->header.offset - sizeof(BMPHeader));
+    const unsigned char *data1 = all_data1 + header1->offset;
+    const unsigned char *data2 = all_data2 + header2->offset;
     int32_t current_byte = 0;
-    int32_t byte_string_length = fst_image->header.width_px * (fst_image->header.bits_per_pixel) / 8;
+    int32_t byte_string_length = header1->width_px * (header1->bits_per_pixel) / 8;
     //num of bytes of each pixel row
     int32_t padding_len = byte_string_length % 4 == 0 ? 0 : 4 - byte_string_length % 4;
-    for (int32_t current_row = 0; current_row < fst_image->header.height_px; ++current_row) {
+    for (int32_t current_row = 0; current_row < header1->height_px; ++current_row) {
         int32_t current_pixel = 0;
-        for (current_pixel = 0; current_pixel + 3 < fst_image->header.width_px; current_pixel += 4) {
+        for (current_pixel = 0; current_pixel + 3 < header1->width_px; current_pixel += 4) {
             for (int32_t pixel = 0; pixel < 4; ++pixel) {
-                int8_t bool_is_second_zero = 1;
-                if (data2[current_byte + 3] != 0) {
-                    bool_is_second_zero = 0;
-                }
-                if (!bool_is_second_zero) {
-                    fwrite(data2 + current_byte, snd_image->header.bits_per_pixel / 8, 1, file_ptr);
-                } else {
-                    fwrite(data1 + current_byte, fst_image->header.bits_per_pixel / 8, 1, file_ptr);
-                }
-                current_byte += fst_image->header.bits_per_pixel / 8;
-            }
-        }
-        /*if (current_pixel != fst_image->header.width_px) {
-            for (int32_t pixel = 0; pixel < fst_image->header.width_px - current_pixel; ++pixel) {
-                int8_t bool_is_second_zero = 1;
-                for (int32_t i = 0; i < fst_image->header.bits_per_pixel / 8; ++i) {
-                    if (snd_image->data[current_byte + i] != 0) {
-                        bool_is_second_zero = 0;
+                unsigned char color[4];
+                double alpha = data2[current_byte + 3] / 255.;
+                for (int i = 0; i < 4; ++i) {
+                    if (i == 3) {
+                        color[3] = 255;
+                    } else {
+                        color[i] = (unsigned char)(data1[current_byte + i] * (1-alpha) + data2[current_byte + i] * alpha);
                     }
                 }
-                if (bool_is_second_zero) {
-                    fwrite(snd_image->data + current_byte, snd_image->header.bits_per_pixel / 8, 1, file_ptr);
-                } else {
-                    fwrite(fst_image->data + current_byte, fst_image->header.bits_per_pixel / 8, 1, file_ptr);
+                fwrite(color, header2->bits_per_pixel / 8, 1, file_ptr);
 
-                }
-                current_byte += fst_image->header.bits_per_pixel / 8;
+                current_byte += header1->bits_per_pixel / 8;
             }
         }
-        current_byte += padding_len;*/
+        if (current_pixel != header1->width_px) {
+            for (int32_t pixel = 0; pixel < header1->width_px - current_pixel; ++pixel) {
+                unsigned char color[4];
+                double alpha = data2[current_byte + 3] / 255.;
+                for (int i = 0; i < 4; ++i) {
+                    if (i == 3) {
+                        color[3] = 255;
+                    } else {
+                        color[i] = (unsigned char)(data1[current_byte + i] * (1-alpha) + data2[current_byte + i] * alpha);
+                    }
+                }
+                fwrite(color, header2->bits_per_pixel / 8, 1, file_ptr);
+
+                current_byte += header1->bits_per_pixel / 8;
+            }
+        }
+        current_byte += padding_len;
 
     }
 }
 
 int main() {
 
-    BMPImage image = ReadBMPImage("/home/egor/C_projects/MergingBMP_SSE/example32.bmp");
-    BMPImage snd = ReadBMPImage("/home/egor/C_projects/MergingBMP_SSE/second.bmp");
-    MergeBMPImages(&image, &snd, "/home/egor/C_projects/MergingBMP_SSE/devil_result.bmp");
-    /*for (size_t i = 0; i < image.header.image_size_bytes; ++i) {
-        printf("%d ", image.data[i]);
-    }
-    printf("\n");
-    for (size_t i = 0; i < snd.header.image_size_bytes; ++i) {
-        printf("%d ", snd.data[i]);
-    }*/
-    printf("\n");
-    printf("%d\n", image.header.height_px);
-    printf("%d\n", snd.header.height_px);
+    const unsigned char *all_data1 = LoadBitmapFile("/home/egor/C_projects/MergingBMP_SSE/example32.bmp");
+    const unsigned char *all_data2 = LoadBitmapFile("/home/egor/C_projects/MergingBMP_SSE/second.bmp");
+    MergeBMPImages(all_data1, all_data2, "/home/egor/C_projects/MergingBMP_SSE/not_devil_res.bmp");
 
-    /*
-    for(size_t i = 0; i < image.header.image_size_bytes; ++i){
-        if(i < image.header.image_size_bytes / 4){
-            image.data[i] = 0;
-        }
-        else if(i < image.header.image_size_bytes / 2){
-            image.data[i] = 115;
-        }
-        else{
-            image.data[i] = 200;
-        }
-    }
 
-    FILE * snd_file = fopen("/home/egor/C_projects/MergingBMP_SSE/second.bmp", "wb");
-    fwrite(&image.header, sizeof(BMPHeader), 1, snd_file);
-    fwrite(image.data, image.header.image_size_bytes, 1, snd_file);
-    printf("%d %d %d \n", image.header.height_px, image.header.width_px, image.header.bits_per_pixel);
-    //now do what you want with it, later on i will show you how to display it in a normal window
 
-    fclose(snd_file);*/
+    const unsigned char *all_data3 = LoadBitmapFile("/home/egor/C_projects/MergingBMP_SSE/devil1.bmp");
+    const unsigned char *all_data4 = LoadBitmapFile("/home/egor/C_projects/MergingBMP_SSE/devil2.bmp");
+    MergeBMPImages(all_data3, all_data4, "/home/egor/C_projects/MergingBMP_SSE/devil_result.bmp");
+
 
 }
