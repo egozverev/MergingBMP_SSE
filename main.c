e339bbcc09e16b5ab30f6466da8330bd5ad4187e
egor <zzwerruga@mail.ru>
2019-11-11 17:30:22 +0300

    Final version

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ typedef struct {        // Total: 54 bytes
     uint16_t reserved1; // Not used
     uint16_t reserved2; // Not used
     uint32_t offset; // Offset to image data in bytes from beginning of file (54
-                     // bytes)
+    // bytes)
     uint32_t dib_header_size;  // DIB Header size in bytes (40 bytes)
     int32_t width_px;          // Width of the image
     int32_t height_px;         // Height of image
@@ -25,10 +25,9 @@ typedef struct {        // Total: 54 bytes
     uint32_t important_colors; // Important colors
 } __attribute__((packed)) BMPHeader;
 
-unsigned char* LoadBitmapFile(const char* filename)
-{
-    FILE* file_ptr;
-    unsigned char* bmp_image_data; // image data pointer, returned in the end
+unsigned char *LoadBitmapFile(const char *filename) {
+    FILE *file_ptr;
+    unsigned char *bmp_image_data; // image data pointer, returned in the end
 
     file_ptr = fopen(filename, "rb"); // open filename in read binary mode
     if (file_ptr ==
@@ -39,51 +38,51 @@ unsigned char* LoadBitmapFile(const char* filename)
     fseek(file_ptr, 0, SEEK_END); /* teleport to the end of the file */
     uint64_t file_length = ftell(file_ptr);
     fseek(file_ptr, 0, SEEK_SET); /* teleport to the beginning */
-    unsigned char* file_all_data = (unsigned char*)_mm_malloc(file_length, 16);
+    unsigned char *file_all_data = (unsigned char *) _mm_malloc(file_length, 16);
     fread(file_all_data, file_length, 1, file_ptr);
     fclose(file_ptr);
     return file_all_data;
 }
 
 void MergeBMPImages(
-    const unsigned char* all_data1,
-    const unsigned char* all_data2,
-    const char* result_filename)
-{
-    FILE* file_ptr = fopen(result_filename, "wb");
-    BMPHeader* header1 = (BMPHeader*)all_data1;
-    BMPHeader* header2 = (BMPHeader*)all_data2;
+        const unsigned char *all_data1,
+        const unsigned char *all_data2,
+        const char *result_filename) {
+    // assuming two images in A - {R,B,G} in any order.
+    FILE *file_ptr = fopen(result_filename, "wb");
+    BMPHeader *header1 = (BMPHeader *) all_data1;
+    BMPHeader *header2 = (BMPHeader *) all_data2;
     fwrite(all_data1, header1->offset, 1, file_ptr);
-    const unsigned char* data1 = all_data1 + header1->offset;
-    const unsigned char* data2 = all_data2 + header2->offset;
+    const unsigned char *data1 = all_data1 + header1->offset;
+    const unsigned char *data2 = all_data2 + header2->offset;
     int32_t current_byte = 0;
     int32_t byte_string_length =
-        header1->width_px * (header1->bits_per_pixel) / 8;
+            header1->width_px * (header1->bits_per_pixel) / 8;
     // num of bytes of each pixel row
     int32_t padding_len =
-        byte_string_length % 4 == 0 ? 0 : 4 - byte_string_length % 4;
+            byte_string_length % 4 == 0 ? 0 : 4 - byte_string_length % 4;
     for (int32_t current_row = 0; current_row < header1->height_px;
          ++current_row) {
         int32_t current_pixel = 0;
         for (current_pixel = 0; current_pixel < header1->width_px;
              current_pixel += 1) {
-            const __m64* data1_vector_ptr =
-                (__m64*)(data1 + current_byte); // unsigned ints!
-            const __m64* data2_vector_ptr = (__m64*)(data2 + current_byte);
+            const __m64 *data1_vector_ptr =
+                    (__m64 *) (data1 + current_byte); // unsigned ints!
+            const __m64 *data2_vector_ptr = (__m64 *) (data2 + current_byte);
             __m128 data1_vector_float = _mm_cvtpu8_ps(*data1_vector_ptr);
             __m128 data2_vector_float = _mm_cvtpu8_ps(*data2_vector_ptr);
-            float alpha = data2_vector_float[3] / 255;
+            float alpha = data2_vector_float[4] / 255;
             __m128 mask = _mm_set_ps1(1 - alpha);
             data1_vector_float = _mm_mul_ps(data1_vector_float, mask);
             mask = _mm_set_ps1(alpha);
             data2_vector_float = _mm_mul_ps(data2_vector_float, mask);
             data1_vector_float =
-                _mm_add_ps(data1_vector_float, data2_vector_float);
+                    _mm_add_ps(data1_vector_float, data2_vector_float);
             data1_vector_float =
-                _mm_round_ps(data1_vector_float, _MM_FROUND_TO_NEAREST_INT);
+                    _mm_round_ps(data1_vector_float, _MM_FROUND_TO_NEAREST_INT);
             unsigned char result[4];
             for (int i = 0; i < 3; ++i) {
-                result[i] = (unsigned char)data1_vector_float[i];
+                result[i] = (unsigned char) data1_vector_float[i];
             }
             result[3] = 255;
             fwrite(result, header2->bits_per_pixel / 8, 1, file_ptr);
@@ -93,9 +92,8 @@ void MergeBMPImages(
     }
 }
 
-int main(int argc, char** argv)
-{
-    const unsigned char* all_data1 = LoadBitmapFile(argv[1]);
-    const unsigned char* all_data2 = LoadBitmapFile(argv[2]);
+int main(int argc, char **argv) {
+    const unsigned char *all_data1 = LoadBitmapFile(argv[1]);
+    const unsigned char *all_data2 = LoadBitmapFile(argv[2]);
     MergeBMPImages(all_data1, all_data2, argv[3]);
 }
